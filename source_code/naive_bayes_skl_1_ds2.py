42901f15e04f303d3e3b9f5dd2cb1f61455c124d
hoangphand <hoangphanduy2015@gmail.com>
2018-10-22 14:33:04 -0400

    added quick implementation using sklearn

---
+++ source_code/naive_bayes_skl_1_ds2.py
@@ -0,0 +1,26 @@
+from __future__ import division
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
+import sys
+import image_util
+
+(train_set, train_label, count_label) = image_util.load_dataset(image_util.DS2_TRAIN_PATH, image_util.DS2_LABEL_SIZE)
+
+# clf = GaussianNB()
+clf = BernoulliNB()
+clf.fit(train_set, train_label)
+
+(val_set, val_label, val_count_label) = image_util.load_dataset(image_util.DS2_VAL_PATH, image_util.DS2_LABEL_SIZE)
+
+predictions = clf.predict(val_set)
+
+correct_count = 0
+for row in range(image_util.DS2_VAL_SIZE):
+	print("prediction: " + str(predictions[row]))
+	print("actual: " + str(val_label[row]))
+
+	if predictions[row] == val_label[row]:
+		correct_count = correct_count + 1
+
+print(correct_count / image_util.DS2_VAL_SIZE)
